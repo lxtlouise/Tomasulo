4055afc63122ecaf3df3277e0d7f0dba86fc4942
xiaotingli <xiaotingli@fq-wifi-10-215-181-210.wireless.pitt.edu>
2018-10-04 18:11:49 -0400

    need output from IF

---
+++ CPU/InstructionDecode.c
@@ -0,0 +1,340 @@
+# include "../Global/TomasuloSimulator.h"
+
+// Instruction * instruction;
+// char* instruction_str;
+
+void decode_instruction();
+void initializeDecode() {
+  instructionQueue = createCircularQueue(config -> NI);
+}
+
+void decode(IF_Unit  *if_unit){
+
+  // for (int i = 0; i < if_unit->n_instructions; i++) {
+  //   Instruction instruction = if_unit->instructions[i];
+  //   decode_instruction(&instruction);
+  //   enque_instrution_queue(&instruction);
+  //   int count = instructionQueue -> getCountCircularQueue;
+  //   printf("%d", count);
+  //
+  //  }
+ }
+
+void decode_instruction(Instruction *instr) {
+  // char *instruction_str = (char *) malloc (sizeof(char) * MAX_LINE);
+  // strcpy (instruction_str, instr -> instr);
+  char* instruction_str = "MUL.D F0, F0, F2";
+
+  Instruction *instruction;
+  char *token;
+  OpCode op;
+
+  int rd;
+  int rs;
+  int rt;
+
+  int rsValue;
+  int rtValue;
+
+  int fd;
+  int fs;
+  int ft;
+
+  double fsValue;
+  double ftValue;
+
+  int immediate;
+
+  int target;
+
+  op = NOOP, rd = -1, rs = -1, rt = -1, rsValue = -1, rtValue = -1, fd = -1, fs = -1, ft = -1, fsValue = -1, ftValue = -1, immediate = 0, target = 0;
+
+	token = (char *) malloc (sizeof(char) * MAX_LINE);
+
+	strcpy (token, instruction_str);
+
+	token = strtok(token, " ,()\t\n");
+
+	if(strcmp(token, "AND") == 0) {
+		op = AND;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+	} else if(strcmp(token, "ANDI") == 0) {
+		op = ANDI;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		immediate = atoi(token);
+	} else if(strcmp(token, "OR") == 0) {
+		op = OR;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+	} else if(strcmp(token, "ORI") == 0) {
+		op = ORI;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		immediate = atoi(token);
+	} else if(strcmp(token, "SLT") == 0) {
+		op = SLT;
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+	} else if(strcmp(token, "SLTI") == 0) {
+		op = SLTI;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		immediate = atoi(token);
+	} else if(strcmp(token, "DADD") == 0) {
+		op = DADD;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+	} else if(strcmp(token, "DADDI") == 0) {
+		op = DADDI;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+
+		immediate = atoi(token);
+	} else if(strcmp(token, "DSUB") == 0) {
+		op = DSUB;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+	} else if(strcmp(token, "DMUL") == 0) {
+		op = DMUL;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+	} else if(strcmp(token, "LD") == 0) {
+		op = LD;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		immediate = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+	} else if(strcmp(token, "SD") == 0) {
+		op = SD;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		immediate = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+	} else if(strcmp(token, "L.D") == 0) {
+		op = L_D;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		ft = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		immediate = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+	} else if(strcmp(token, "S.D") == 0) {
+		op = S_D;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		ft = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		immediate = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+	} else if(strcmp(token, "ADD.D") == 0) {
+		op = ADD_D;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		ft = atoi(token);
+	} else if(strcmp(token, "SUB.D") == 0) {
+		op = SUB_D;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		ft = atoi(token);
+	} else if(strcmp(token, "MUL.D") == 0) {
+		op = MUL_D;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		ft = atoi(token);
+	} else if(strcmp(token, "DIV.D") == 0) {
+		op = DIV_D;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fd = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		fs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		ft = atoi(token);
+	} else if(strcmp(token, "BEQZ") == 0) {
+		op = BEQZ;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()\t\n");
+	} else if(strcmp(token, "BNEZ") == 0) {
+		op = BNEZ;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()\t\n");
+	} else if(strcmp(token, "BEQ") == 0) {
+		op = BEQ;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+
+		token = strtok(NULL, " ,()\t\n");
+	} else if(strcmp(token, "BNE") == 0) {
+		op = BNE;
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rs = atoi(token);
+
+		token = strtok(NULL, " ,()RF\t\n");
+		rt = atoi(token);
+
+		token = strtok(NULL, " ,()\t\n");
+	} else {
+		printf("Invalid instruction %s...\n", instruction_str);
+		exit (EXIT_FAILURE);
+	}
+
+	if (op == BEQZ || op == BNEZ || op == BEQ || op == BNE) {
+		DictionaryEntry *codeLabel = getValueChainByDictionaryKey (codeLabels, (void *) token);
+
+		if (codeLabel == NULL) {
+			printf("Invalid code label cannot be resolved...\n");
+			exit (EXIT_FAILURE);
+		} else {
+			target = *((int*)codeLabel -> value -> value);
+		}
+	}
+
+	instruction = (Instruction *) malloc (sizeof(Instruction));
+
+	instruction -> op = op;
+
+	instruction -> rd = rd;
+	instruction -> rs = rs;
+	instruction -> rt = rt;
+
+	instruction -> rsValue = rsValue;
+	instruction -> rtValue = rtValue;
+
+	instruction -> fd = fd;
+	instruction -> fs = fs;
+	instruction -> ft = ft;
+
+	instruction -> fsValue = fsValue;
+	instruction -> ftValue = ftValue;
+
+	instruction -> immediate = immediate;
+
+	instruction -> target = target;
+
+	printf("Decoded %d:%s -> %s, rd=%d, rs=%d, rt=%d, fd=%d, fs=%d, ft=%d, immediate=%d, target=%d\n", cpu -> PC, instruction_str,
+		 getOpcodeString ((int) op), rd, rs, rt, fd, fs, ft, immediate, target);
+}
+
+void enque_instrution_queue(Instruction *instruction) {
+  instruction = (Instruction *) malloc(sizeof(Instruction));
+  if (isFullCircularQueue(instructionQueue)) {
+    printf('Instrution queue is full.');
+    return;
+  } else {
+    enqueueCircular(instructionQueue, instruction);
+  }
+}
